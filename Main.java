02f1f53a5dcffbbf00c49426cbc8c67eae829c33
Ana Júlia Leite <leite.anajulia25@gmail.com>
2024-02-27 21:47:11 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    int idade = 31;
+    String nome = "AnaJulia";
+    float altura = 1.61f;
+    double decimal = 1.61;
+    char letrainicial = 'A';
+    boolean EusouAnaJulia = true;
+
+    System.out.println("idade = " + idade);
+    System.out.println("nome = " + nome);
+    System.out.println("altura = " + altura);
+    System.out.println("decimal = " + decimal);
+    System.out.println("letrainicial = " + letrainicial);
+    System.out.println("EusouAnaJulia = " + EusouAnaJulia);
+    
+  }
+  
+}
